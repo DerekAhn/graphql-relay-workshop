2e4855f32e846a094dc4cd8c099bea406cb2dc04
DerekAhn <git.derek@gmail.com>
2016-07-28 12:15:02 -0700

    Using humps.js camelizeKeys for db formatting
    - external layer to format data to be js consistent.

--- schema.js
+++ schema.js
@@ -7,15 +7,19 @@ const {
   GraphQLInt,
   GraphQLList,
   GraphQLID
+
 } = require('graphql');
 
-const person = {
+const humps = require('humps');
+
+
+const person = humps.camelizeKeys({
   id: 1,
   first_name: 'Derek',
   last_name: 'Ahn',
   email: 'derek@webmocha.com',
   spouse_id: 8
-};
+});
 
 const personType = new GraphQLObjectType({
   name: 'Person',
@@ -25,19 +29,16 @@ const personType = new GraphQLObjectType({
       type: GraphQLID
     },
     firstName: {
-      type: GraphQLString,
-      resolve: obj => obj.first_name
+      type: GraphQLString
     },
     lastName: {
-      type: GraphQLString,
-      resolve: obj => obj.last_name
+      type: GraphQLString
     },
     email: {
       type: GraphQLString
     },
     spouseId: {
-      type: GraphQLInt,
-      resolve: obj => obj.spouse_id
+      type: GraphQLInt
     }
   }
 });
